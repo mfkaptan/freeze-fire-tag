0aaa6b9b4f3886b9096c895b2d1a7ad653e4e79c
mfkaptan <kaptanmf@gmail.com>
2015-01-01 14:01:47 +0200

    Assets and DesktopLauncher

--- freeze-fire-tag/desktop/src/com/kaptan/freezefiretag/desktop/DesktopLauncher.java
+++ freeze-fire-tag/desktop/src/com/kaptan/freezefiretag/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kaptan.freezefiretag.FreezeFireTag;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FreezeFireTag(), config);
-	}
+
+public class DesktopLauncher
+{
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new FreezeFireTag(), config);
+    }
 }
