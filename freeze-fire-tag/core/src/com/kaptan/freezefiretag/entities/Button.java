acb451613ddff7a5a8188c8f49a61abafa63e408
mfkaptan <kaptanmf@gmail.com>
2015-01-11 15:24:17 +0100

    Remove unused variable

--- freeze-fire-tag/core/src/com/kaptan/freezefiretag/entities/Button.java
+++ freeze-fire-tag/core/src/com/kaptan/freezefiretag/entities/Button.java
@@ -8,12 +8,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Button extends TextButton
 {
-    private final Board board;
-
     public Button(String text, Skin skin, final Board board)
     {
         super(text, skin);
-        this.board = board;
         addListener(new InputListener()
         {
             public boolean touchDown(InputEvent event, float x, float y,
